e514e234eb244651244d70b4fb38ceea7dc07922
Alexandra <alex_gorskaya1@mail.ru>
2021-11-26 23:34:54 +0300

    done

--- script.js
+++ script.js
@@ -37,48 +37,52 @@ tree.textContent = 'Tree';
 document.body.append(tree);
 const container = document.createElement('div');
 document.body.append(container);
-
-function createchild(menu, container) {
-	const mainul = document.createElement('ul');
-	container.append(mainul);
-	container.onclick = null;
+function createChild(menu, container) {
+	const mainUl = document.createElement('ul');
+	container.append(mainUl);
 	menu.forEach((item) => {
 		const listItem = document.createElement('li');
-		listItem.classList.add('plus');
-		listItem.classList.add('bold');
+		listItem.classList.add('plus', 'bold');
 		listItem.textContent = `${item.title}`;
-		mainul.append(listItem);
+		mainUl.append(listItem);
+
 		if (item.nodes.length) {
-			listItem.onclick = (e) => {
-				listItem.classList.remove('plus');
-				listItem.classList.add('minus');
-				mainul.classList.add(`${listItem.textContent}`);
-				createchild(item.nodes, listItem);
-			};
+			createChild(item.nodes, listItem);
 		}
 		if (!item.nodes.length) {
-			listItem.onclick = (e) => {
-				console.log(e.target);
-				if (
-					e.target.classList.contains('minus') ||
-					e.target.classList.contains('non-bold') ||
-					e.target.classList.contains('child')
-				) {
-					e.stopPropagation();
-				} else {
-					listItem.classList.remove('plus');
-					listItem.classList.add('minus');
-					const childul = document.createElement('ul');
-					const childli = document.createElement('li');
-					childli.textContent = 'node';
-					childli.classList.add('non-bold');
-					childul.classList.add('child');
-					listItem.append(childul);
-					childul.append(childli);
-				}
-			};
+			const newUl = document.createElement('ul');
+			const newLi = document.createElement('li');
+			newLi.textContent = 'node';
+			newLi.classList.add('non-bold');
+			listItem.append(newUl);
+			newUl.append(newLi);
 		}
 	});
 }
-createchild(menu, container);
-const ullarr = document.getElementsByClassName('minus');
+function openElement(e) {
+	e.target.classList.toggle('minus');
+	e.target.classList.toggle('plus');
+	for (let child of e.target.children) {
+		child.classList.toggle('display-none');
+	}
+}
+
+function addEvent() {
+	const allLi = document.getElementsByClassName('bold');
+	console.log(allLi);
+	Array.from(allLi).forEach((element) => {
+		for (let li of allLi) {
+			for (let uls of li.children) uls.classList.add('display-none');
+		}
+		element.onclick = (e) => {
+			if (!e.target.classList.contains('non-bold')) {
+				e.stopPropagation();
+				console.log('hi');
+				openElement(e);
+			}
+		};
+	});
+}
+
+createChild(menu, container);
+addEvent();
